8e56857331bef4ec27571d51b083eeac6ae89710
NaderBhrr <nader.bhr.devv@gmail.com>
2020-09-22 15:14:49 +0330

    Refactor the validation module with functions

--- routers/auth.js
+++ routers/auth.js
@@ -4,11 +4,6 @@ const router = require("express").Router();
 const User = require("../model/User_model");
 
 router.post("/register", async (req, res) => {
-  // Data should be validated before adding new users
-  const { error } = Joi.validate(req.body, schema);
-
-  return error ? res.status(400).send(error.details[0].message) : null;
-
   const user = new User({
     name: req.body.name, // This data come from the request body
     email: req.body.email,
--- validation.js
+++ validation.js
@@ -1,8 +1,15 @@
 // Validation logic
 const Joi = require("@hapi/joi");
 
-const schema = {
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
+// Register validation function
+const registerValidation = (newUserData) => {
+  const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  };
+
+  return Joi.validate(newUserData, schema);
 };
+
+module.exports.registerValidation = registerValidation;
