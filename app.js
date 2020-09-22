2058dc5564c45c92024484bd2559812dbb08d998
NaderBhrr <nader.bhr.devv@gmail.com>
2020-09-22 13:33:21 +0330

    Add the user object to the POST request in auth.js

--- app.js
+++ app.js
@@ -4,18 +4,23 @@
 // Adding Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-
-// Connect to database
-mongoose.connect("", () => {
-  console.log("Application connected successfully to DB");
-});
-
+const dotenv = require("dotenv");
 //  Import routes
 const authRoute = require("./routers/auth");
+
+dotenv.config();
+
+// Connect to database
+mongoose.connect(process.env.DB_ADDRESS, () => {
+  console.log("Application connected successfully to DB");
+});
 
 // Basic variables
 const app = express();
 const port = process.env.PORT || 1337;
+
+// Middleware functionalities
+app.use(express.json());
 
 // Creating route middlewares
 app.use("/api/users", authRoute);
--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,21 @@
 // The file for the authentication routes
 
 const router = require("express").Router();
+const User = require("../model/User_model");
 
-router.post("/register", (req, res) => {
-  res.send("Registeration");
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name, // This data come from the request body
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
